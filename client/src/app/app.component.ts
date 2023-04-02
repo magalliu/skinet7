1bc9f957fa1a84ca6671bb17918fea47e30c4c20
magalliu <agalliumarjol8@gmail.com>
2023-04-02 21:07:15 +0200

    End of Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Skinet';
+  //products: any[] =[];
+  products:Product[] = [];
+
+  constructor(private http:HttpClient){}
+
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      //next:response=>console.log(response),//what to do next
+      next:(response)=>this.products=response.data,
+      error:error=>console.log(error),//what to do id there is an error
+      complete: ()=>{
+        console.log('requested completed');
+        console.log('extra statment');
+
+      }
+    })
+  }
 }
---
+++ client/src/demo.ts
@@ -0,0 +1,35 @@
+
+/*
+let data: number | string;
+
+
+data='42';
+
+data=10;
+
+interface ICar
+{
+color:string;
+model:string;
+topSpeed?:number
+
+};
+
+const car1:ICar={
+    color:'blue',
+    model : 'bmw'
+};
+
+const car2 :ICar ={
+    color:'blue',
+    model : 'mercedes',
+    topSpeed:100
+};
+
+const multiply = (x: number,y: number) :string => {
+
+     return (x * y).toString();
+
+};
+
+*/
